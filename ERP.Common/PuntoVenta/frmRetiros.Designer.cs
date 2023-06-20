1ce71f58c7b9ca032d462c90de4a9cdfa62e8353
RodoStark <30399857+RodoStark@users.noreply.github.com>
2023-06-20 13:16:25 -0600

    Cambios en la clase retiro (diseño y estructura de la clase)
    Cambios en la clase retiro (diseño y estructura de la clase)

--- ERP.Common/PuntoVenta/frmRetiros.Designer.cs
+++ ERP.Common/PuntoVenta/frmRetiros.Designer.cs
@@ -28,93 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRetiros));
             this.label1 = new System.Windows.Forms.Label();
-            this.uiMonto = new System.Windows.Forms.NumericUpDown();
-            this.uiObservaciones = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.uiMonto)).BeginInit();
+            this.uiMonto = new DevExpress.XtraEditors.SpinEdit();
+            this.uiCancelar = new DevExpress.XtraEditors.SimpleButton();
+            this.uiGuardar = new DevExpress.XtraEditors.SimpleButton();
+            this.uiObservaciones = new DevExpress.XtraEditors.MemoEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiMonto.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiObservaciones.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 15);
+            this.label1.Location = new System.Drawing.Point(68, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(47, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Monto";
-            // 
-            // uiMonto
-            // 
-            this.uiMonto.DecimalPlaces = 2;
-            this.uiMonto.Enabled = false;
-            this.uiMonto.Location = new System.Drawing.Point(94, 13);
-            this.uiMonto.Maximum = new decimal(new int[] {
-            1410065407,
-            2,
-            0,
-            0});
-            this.uiMonto.Name = "uiMonto";
-            this.uiMonto.Size = new System.Drawing.Size(120, 20);
-            this.uiMonto.TabIndex = 1;
-            // 
-            // uiObservaciones
-            // 
-            this.uiObservaciones.Location = new System.Drawing.Point(94, 41);
-            this.uiObservaciones.Multiline = true;
-            this.uiObservaciones.Name = "uiObservaciones";
-            this.uiObservaciones.Size = new System.Drawing.Size(276, 82);
-            this.uiObservaciones.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.labelMonto_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Location = new System.Drawing.Point(16, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(103, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Observaciones";
+            this.label2.Click += new System.EventHandler(this.labelObservaciones_Click);
             // 
-            // button1
+            // uiMonto
             // 
-            this.button1.Location = new System.Drawing.Point(94, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "REGISTRAR";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.uiMonto.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.uiMonto.Location = new System.Drawing.Point(125, 15);
+            this.uiMonto.Name = "uiMonto";
+            this.uiMonto.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.uiMonto.Size = new System.Drawing.Size(193, 22);
+            this.uiMonto.TabIndex = 6;
+            this.uiMonto.EditValueChanged += new System.EventHandler(this.uiMonto_EditValueChanged);
             // 
-            // button2
+            // uiCancelar
             // 
-            this.button2.Location = new System.Drawing.Point(220, 129);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "CANCELAR";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.uiCancelar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("uiCancelar.ImageOptions.Image")));
+            this.uiCancelar.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.uiCancelar.Location = new System.Drawing.Point(314, 148);
+            this.uiCancelar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.uiCancelar.Name = "uiCancelar";
+            this.uiCancelar.Size = new System.Drawing.Size(178, 40);
+            this.uiCancelar.TabIndex = 11;
+            this.uiCancelar.Text = "Cancelar";
+            this.uiCancelar.Click += new System.EventHandler(this.CancelarRetiro_Click);
+            // 
+            // uiGuardar
+            // 
+            this.uiGuardar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("uiGuardar.ImageOptions.Image")));
+            this.uiGuardar.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.uiGuardar.Location = new System.Drawing.Point(130, 148);
+            this.uiGuardar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.uiGuardar.Name = "uiGuardar";
+            this.uiGuardar.Size = new System.Drawing.Size(178, 40);
+            this.uiGuardar.TabIndex = 10;
+            this.uiGuardar.Text = "Guardar";
+            this.uiGuardar.Click += new System.EventHandler(this.GuadarRetiros);
+            // 
+            // uiObservaciones
+            // 
+            this.uiObservaciones.Location = new System.Drawing.Point(126, 58);
+            this.uiObservaciones.Name = "uiObservaciones";
+            this.uiObservaciones.Size = new System.Drawing.Size(366, 75);
+            this.uiObservaciones.TabIndex = 13;
+            this.uiObservaciones.EditValueChanged += new System.EventHandler(this.uiObservaciones_EditValueChanged);
             // 
             // frmRetiros
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 154);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(527, 201);
             this.Controls.Add(this.uiObservaciones);
+            this.Controls.Add(this.uiCancelar);
+            this.Controls.Add(this.uiGuardar);
             this.Controls.Add(this.uiMonto);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.MaximumSize = new System.Drawing.Size(413, 210);
-            this.MinimumSize = new System.Drawing.Size(413, 210);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(545, 248);
+            this.MinimumSize = new System.Drawing.Size(545, 248);
             this.Name = "frmRetiros";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Retiros";
             this.Load += new System.EventHandler(this.frmRetiros_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.uiMonto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiMonto.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiObservaciones.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,10 +136,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown uiMonto;
-        private System.Windows.Forms.TextBox uiObservaciones;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private DevExpress.XtraEditors.SpinEdit uiMonto;
+        private DevExpress.XtraEditors.SimpleButton uiCancelar;
+        private DevExpress.XtraEditors.SimpleButton uiGuardar;
+        private DevExpress.XtraEditors.MemoEdit uiObservaciones;
     }
 }
--- ERP.Common/PuntoVenta/frmRetiros.cs
+++ ERP.Common/PuntoVenta/frmRetiros.cs
@@ -1,15 +1,9 @@
 ﻿using ConexionBD;
 using ConexionBD.Models;
-using ERP.Common.Reports;
 using ERP.Reports;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ERP.Common.PuntoVenta
@@ -20,12 +14,12 @@ namespace ERP.Common.PuntoVenta
         public PuntoVentaContext puntoVentaContext;
         int err=0;
         public List<DeclaracionFondoModel> lstDenominaciones;
+        BarraCargarForms welcome;
 
         public frmRetiros()
         {
             InitializeComponent();
-            oContext = new ERPProdEntities();
-            
+            oContext = new ERPProdEntities();   
         }
 
         private void AbrirDeclaracionFondo()
@@ -42,11 +36,9 @@ namespace ERP.Common.PuntoVenta
                     this.uiMonto.Value = oForm.model2.Sum(s => s.total??0);
                     lstDenominaciones = oForm.model2;
                 }
-
             }
             catch (Exception ex)
             {
-
                 err = ERP.Business.SisBitacoraBusiness.Insert(puntoVentaContext.usuarioId,
                                   "ERP",
                                   this.Name,
@@ -64,10 +56,10 @@ namespace ERP.Common.PuntoVenta
         {
             try
             {       
-                if(lstDenominaciones.Count() == 0)
-                {
-                    AbrirDeclaracionFondo();
-                }
+                //if(lstDenominaciones.Count() == 0)
+                //{
+                    //AbrirDeclaracionFondo();
+                //}
                 
                 oContext = new ERPProdEntities();
 
@@ -89,25 +81,46 @@ namespace ERP.Common.PuntoVenta
                             ERP.Utils.MessageBoxUtil.ShowWarning("Aún no se dispone del efectivo solicitado, efectivo disponible $" + string.Format("{0:C}", disponibleEfectivo));
                             return;
                         }
-
                     }
                 }
-                
+
+                if (uiMonto.Value < 0)
+                {
+                    //XtraMessageBox.Show("Es necesario ingresar cantidades mayores a cero", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    // MessageBox.Show("Es necesario ingresar cantidades mayores a cero", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    ERP.Utils.MessageBoxUtil.ShowError("Es necesario ingresar cantidades mayores a cero");
+                    return;
+                }
+                if (uiObservaciones.Text.Length >= 250)
+                {
+                    //XtraMessageBox.Show("Es necesario ingresar texto menor a 250 caracteres", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    //MessageBox.Show("Es necesario ingresar texto menor a 250 caracteres ", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ERP.Utils.MessageBoxUtil.ShowError("Es necesario ingresar texto menor a 250 caracteres");
+                    return;
+                }
+
+                int monto = (int)uiMonto.Value;
+                string observaciones = uiObservaciones.Text;
 
                 doc_retiros entity = new doc_retiros();
                 entity.CajaId = this.puntoVentaContext.cajaId;
                 entity.CreadoPor = this.puntoVentaContext.usuarioId;
                 entity.FechaRetiro = this.oContext.p_GetDateTimeServer().FirstOrDefault().Value;
-                entity.MontoRetiro = uiMonto.Value;
-                entity.Observaciones = uiObservaciones.Text;
+                entity.MontoRetiro = monto;
+                entity.Observaciones = observaciones;
                 entity.RetiroId = this.oContext.doc_retiros.Count() > 0 ? this.oContext.doc_retiros.Max(m => m.RetiroId) + 1 : 1;
                 entity.SucursalId = this.puntoVentaContext.sucursalId;
 
                 oContext.doc_retiros.Add(entity);
                 oContext.SaveChanges();
+                
+                welcome = new BarraCargarForms();
+                welcome.setProgress(60);
+                welcome.Show();
+                welcome.BringToFront();
 
                 //Declaracion de Fondo
-                foreach (var itemDeno in lstDenominaciones)
+                /*foreach (var itemDeno in lstDenominaciones)
                 {
                     doc_retiros_denominaciones oRetiroDenominacion = new doc_retiros_denominaciones();
 
@@ -121,43 +134,63 @@ namespace ERP.Common.PuntoVenta
 
                     oContext.doc_retiros_denominaciones.Add(oRetiroDenominacion);
                     oContext.SaveChanges();
+                }*/
 
-
-                }
-
-                MessageBox.Show("RETIRO REGISTRADO", "AVISO");
-
-
+                //MessageBox.Show("RETIRO REGISTRADO", "AVISO");
                 rptRetiroTicket oTicket = new rptRetiroTicket();
-
-                ReportViewer oViewer = new ReportViewer(this.puntoVentaContext.cajaId);
-
-                oTicket.DataSource = oContext.p_rpt_retiro_ticket(entity.RetiroId).ToList();
-
-                oViewer.ShowTicket(oTicket);
-               // oViewer.Show();
-
-
+                //ReportViewer oViewer = new ReportViewer(this.puntoVentaContext.cajaId);
+                //oTicket.DataSource = oContext.p_rpt_retiro_ticket(entity.RetiroId).ToList();
+                //oViewer.ShowTicket(oTicket);
+                //oViewer.Show();
 
                 this.Close();
-
-                
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "ERROR");
-                this.Close();
+                ERP.Utils.MessageBoxUtil.ShowError(ex.Message);
+                //MessageBox.Show(ex.Message, "ERROR");
+                //this.Close();
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            guardar();
         }
 
         private void frmRetiros_Load(object sender, EventArgs e)
         {
-            AbrirDeclaracionFondo();
+            //AbrirDeclaracionFondo();
+        }
+
+        private void uiGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuadarRetiros(object sender, EventArgs e)
+        {
+            guardar();
+        }
+
+        private void CancelarRetiro_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void labelObservaciones_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiMonto_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelMonto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiObservaciones_EditValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
